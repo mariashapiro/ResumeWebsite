5ccea500c1d398dcef32845f6875498648beac42
Maria Shapiro <mariaashapiro@gmail.com>
2021-06-08 19:16:14 -0600

    updated about text

--- my-app/src/components/about.jsx
+++ my-app/src/components/about.jsx
@@ -13,8 +13,9 @@ export default class About extends Component {
                     <div className="about-desc">
                       <span className="heading-meta">About Me</span>
                       <h2 className="colorlib-heading">Who Am I?</h2>
-                      <p>I am an undergraduate student at Georgia Institute of Technology studying Computer Science with threads in Info Networks and People. Right now, I am deciding between pursuing a traditional software engineering role versus a program mananger role.</p>
-                      <p>I am originally from Maryland, close to the DC border, and I am an absolute fiend for Fall weather, anything with wheels (manual cars! longboarding! quad-skating!) and plants <i className="icon-heart" aria-hidden="true" /></p>
+                      <p>I am an undergraduate student at Georgia Institute of Technology studying Computer Science with threads in Info Networks and People. Right now, I am deciding between pursuing a traditional software engineering role versus a program mananger role.
+                        The plan is to graduate in Spring 2022, but before that I plan to study abroad in Fall 2021 in Seoul, South Korea at Yonsei University.</p>
+                      <p>I am originally from Maryland, super close to the DC border, and I am an absolute fiend for Fall weather, anything with wheels (manual cars! longboarding! quad-skating!) and plants <i className="icon-heart" aria-hidden="true" /></p>
                     </div>
                   </div>
                 </div>
