aec0112daff410670520bcd8fd3e75c8f266202b
Maria Shapiro <mariaashapiro@gmail.com>
2021-10-27 01:07:36 +0900

    changed background pic and font a little, plus updated summer 2021 experience

---
+++ my-app/src/components/blog.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-work" data-section="Blog">
+        <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Blog</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            </section>
+            </div>)}}
