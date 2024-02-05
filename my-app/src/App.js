daa9e6645a7adfb34a742c237b90a821515af8b4
Maria S <git@kora.anonaddy.me>
2024-02-04 17:47:30 -0800

    add analytics to root

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Timeline from './components/timeline'
-import Activities from './components/activities'
-import Hobbies from './components/hobbies'
-import Projects from './components/projects'
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Introduction from "./components/introduction";
+import About from "./components/about";
+import Timeline from "./components/timeline";
+import Activities from "./components/activities";
+import Hobbies from "./components/hobbies";
+import Projects from "./components/projects";
+import { Analytics } from "@vercel/analytics/react";
 
 class App extends Component {
   render() {
     return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-          <Sidebar></Sidebar>
-          <div id="colorlib-main">
-            <Introduction></Introduction>
-            <About></About>
-            {/* <Hobbies></Hobbies> */}
-            <Timeline></Timeline>
-            {/* <Activities></Activities> */}
-            {/* <Projects></Projects> */}
-            {/* <Blog></Blog> */}
+      <>
+        <div id="colorlib-page">
+          <div id="container-wrap">
+            <Sidebar></Sidebar>
+            <div id="colorlib-main">
+              <Introduction></Introduction>
+              <About></About>
+              {/* <Hobbies></Hobbies> */}
+              <Timeline></Timeline>
+              {/* <Activities></Activities> */}
+              {/* <Projects></Projects> */}
+              {/* <Blog></Blog> */}
+            </div>
           </div>
         </div>
-      </div>
+        <Analytics />
+      </>
     );
   }
 }
